328e3d19f7a3e28906567deda4e9975a9f076349
patelkarishma10 <50954912+patelkarishma10@users.noreply.github.com>
2019-07-16 17:28:03 +0100

    Update AccountController.java

--- server/src/main/java/com/qa/rest/AccountController.java
+++ server/src/main/java/com/qa/rest/AccountController.java
@@ -29,7 +29,7 @@ public class AccountController {
 	@RequestMapping("/getAllAccounts")
 	public List<Account> getAllAccounts() {
 
-		return "{\"message\": \"account has been successfully updated\"}";
+		return "works";
 // 		return service.getAllAccounts();
 	}
 
